c261df8611761c6799b509cdfe2e427d4b92c8b5
ReivaxD <xavier.argentines@hotmail.com>
2026-05-20 13:51:41 +0200

    v 1.5.0

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 """
-Point d'entrée principal de l'application d'échecs.
+Point d'entrée principal de ChessOP.
 """
 import sys
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtCore import Qt
-from ui.main_window import MainWindow
+from ui.main_launcher import MainLauncher
 
 
 def main():
     app = QApplication(sys.argv)
     app.setApplicationName("ChessOP")
-    app.setApplicationVersion("1.3.0")
+    app.setApplicationVersion("1.5.0")
 
-    window = MainWindow()
-    window.show()
+    launcher = MainLauncher()
+    launcher.show()
 
     sys.exit(app.exec())
 
